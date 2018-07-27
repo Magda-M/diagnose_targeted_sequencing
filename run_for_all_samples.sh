2f32dd25d1450c225911c9eb730e2d90de5f56db
Magda <m.machnicka@mimuw.edu.pl>
2018-07-27 16:50:08 +0200

    correct run all

--- run_for_all_samples.sh
+++ run_for_all_samples.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for l in /mnt/chr11/Data/magda/Powroty/panel/fastq/MARCEL*_001.fastq;
-   do sample=$(echo $l | cut -d_R -f1);
+   do sample=$(echo $l | cut -d_ -f1-3);
    echo $sample;
    done
 
