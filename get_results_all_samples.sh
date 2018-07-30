ec40dc16a1f28145318649b8bd09be55feb9c5dd
Magda <m.machnicka@mimuw.edu.pl>
2018-07-30 12:58:31 +0200

    additional step commented out

--- get_results_all_samples.sh
+++ get_results_all_samples.sh
@@ -8,9 +8,11 @@ for l in "$RESULTS_PATH"/Powroty/panel/fastq/MARCEL*R1_001.fastq;
    #do path=$(echo $l | cut -d/ -f8); ON THE LAPTOP
    sample=$(echo $path | cut -d_ -f1-3);
    echo $sample;
-   #must remove beginning of log because logs from two runs were contatenated
-   mv "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old;
-   tail -n +112063 "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old > "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log;
-   rm "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old;
+
+   ##must remove beginning of log because logs from two runs were contatenated
+   #mv "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old;
+   #tail -n +112063 "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old > "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log;
+   #rm "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old;
+
    python extract_results.py $sample 
    done
