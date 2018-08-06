7aea7ec65cce303c2de0f503970b66987bb1220f
Magda <m.machnicka@mimuw.edu.pl>
2018-08-06 15:57:16 +0200

    added option for merged bams

--- get_results_all_samples.sh
+++ get_results_all_samples.sh
@@ -3,16 +3,24 @@
 RESULTS_PATH="/mnt/chr11/Data/magda"
 #RESULTS_PATH="/home/magda/Dane"  ON THE LAPTOP
 
-for l in "$RESULTS_PATH"/Powroty/panel/fastq/MARCEL*R1_001.fastq;
-   do path=$(echo $l | cut -d/ -f9); 
-   #do path=$(echo $l | cut -d/ -f8); ON THE LAPTOP
-   sample=$(echo $path | cut -d_ -f1-3);
-   echo $sample;
+##lanes separately
+#for l in "$RESULTS_PATH"/Powroty/panel/fastq/MARCEL*R1_001.fastq;
+#   do path=$(echo $l | cut -d/ -f9); 
+#   #do path=$(echo $l | cut -d/ -f8); ON THE LAPTOP
+#   sample=$(echo $path | cut -d_ -f1-3);
+#   echo $sample;
 
    ##must remove beginning of log because logs from two runs were contatenated
    #mv "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old;
    #tail -n +112063 "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old > "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log;
    #rm "$RESULTS_PATH"/Powroty/panel/diagnose_sequencing/results/"$sample".log.old;
 
-   python extract_results.py $sample 
+#   python extract_results.py $sample 
+#   done
+
+#merged lines
+samples_array=( "MARCEL1" "MARCEL2" "MARCEL3" "MARCEL4" )
+for sample in "${samples_array[@]}"
+   do
+   python extract_results.py $sample
    done
