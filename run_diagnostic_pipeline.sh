0526a8e059574dc83773a25676526668310ea3f1
Magda <m.machnicka@mimuw.edu.pl>
2018-08-07 10:48:24 +0200

    For minlen50 and merged lanes

--- run_diagnostic_pipeline.sh
+++ run_diagnostic_pipeline.sh
@@ -5,7 +5,7 @@
 
 SAMPLE_NAME="$1"
 FASTQ_PATH="/mnt/chr11/Data/magda/Powroty/panel/fastq/"
-RESULTS_PATH="/mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/merged/"
+RESULTS_PATH="/mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/merged/minlen50/"
 PRIMARY_TARGET="/mnt/chr11/Data/magda/Powroty/panel/Symfonia_v2_primary_targets.bed"
 CAPTURE_TARGET="/mnt/chr11/Data/magda/Powroty/panel/Symfonia_v2_capture_targets.bed"
 PADDED_CAPTURE_TARGET="/mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/Symfonia_v2_capture_targets_padded.bed"
@@ -13,20 +13,46 @@ PADDED_CAPTURE_TARGET="/mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/r
 echo "\n####Process sample:" "$SAMPLE_NAME"
 
 #echo "\n####Unpack"
-#gunzip "$FASTQ_PATH$SAMPLE_NAME"_R1_001.fastq.gz
-#gunzip "$FASTQ_PATH$SAMPLE_NAME"_R2_001.fastq.gz
+#gunzip "$FASTQ_PATH$SAMPLE_NAME"_L001_R1_001.fastq.gz
+#gunzip "$FASTQ_PATH$SAMPLE_NAME"_L001_R2_001.fastq.gz
+#gunzip "$FASTQ_PATH$SAMPLE_NAME"_L002_R1_001.fastq.gz
+#gunzip "$FASTQ_PATH$SAMPLE_NAME"_L002_R2_001.fastq.gz
 
-#echo "\n####Trimming bad reads and getting read of adapters"
-#java -Xms4g -Xmx4g -jar /home/magda/Trimmomatic-0.38/trimmomatic-0.38.jar PE -threads 20 -phred33 "$FASTQ_PATH$SAMPLE_NAME"_R1_001.fastq "$FASTQ_PATH$SAMPLE_NAME"_R2_001.fastq "$FASTQ_PATH$SAMPLE_NAME"_R1_trimmed.fastq "$FASTQ_PATH$SAMPLE_NAME"_R1_unpaired.fastq "$FASTQ_PATH$SAMPLE_NAME"_R2_trimmed.fastq "$FASTQ_PATH$SAMPLE_NAME"_R2_unpaired.fastq ILLUMINACLIP:/home/magda/Trimmomatic-0.38/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:5:20 MINLEN:75
+echo "\n####Trimming bad reads and getting read of adapters - lane 1"
+java -Xms4g -Xmx4g -jar /home/magda/Trimmomatic-0.38/trimmomatic-0.38.jar PE -threads 20 -phred33 \
+   "$FASTQ_PATH$SAMPLE_NAME"_L001_R1_001.fastq \
+   "$FASTQ_PATH$SAMPLE_NAME"_L001_R2_001.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L001_R1_trimmed_minlen50.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L001_R1_unpaired_minlen50.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L001_R2_trimmed_minlen50.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L001_R2_unpaired_minlen50.fastq \
+   ILLUMINACLIP:/home/magda/Trimmomatic-0.38/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:5:20 MINLEN:50
 
-#echo "\n####Aligning reads"
-#/home/magda/bwa/bwa mem -R '@RG\tID:id\tPL:illumina\tLB:NIMBLEGEN\tSM:$SAMPLE_NAME' /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa -t 28 -M "$FASTQ_PATH$SAMPLE_NAME"_R1_trimmed.fastq "$FASTQ_PATH$SAMPLE_NAME"_R2_trimmed.fastq | samtools view -Sb - > "$RESULTS_PATH$SAMPLE_NAME"_unsorted.bam
+echo "\n####Trimming bad reads and getting read of adapters - lane 2"
+java -Xms4g -Xmx4g -jar /home/magda/Trimmomatic-0.38/trimmomatic-0.38.jar PE -threads 20 -phred33 \
+   "$FASTQ_PATH$SAMPLE_NAME"_L002_R1_001.fastq \
+   "$FASTQ_PATH$SAMPLE_NAME"_L002_R2_001.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L002_R1_trimmed_minlen50.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L002_R1_unpaired_minlen50.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L002_R2_trimmed_minlen50.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L002_R2_unpaired_minlen50.fastq \
+   ILLUMINACLIP:/home/magda/Trimmomatic-0.38/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:5:20 MINLEN:50
 
-#echo "\n####Sort bam file"
-#samtools sort "$RESULTS_PATH$SAMPLE_NAME"_unsorted.bam -o "$RESULTS_PATH$SAMPLE_NAME"_sorted.bam
+echo "\n####Aligning reads - lane 2"
+/home/magda/bwa/bwa mem -R '@RG\tID:'"$SAMPLE_NAME"'_L002\tPL:illumina\tLB:NIMBLEGEN\tSM:'"$SAMPLE_NAME" \
+   /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa -t 40 \
+   -M "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L002_R1_trimmed_minlen50.fastq \
+   "$FASTQ_PATH"/minlen50/"$SAMPLE_NAME"_L002_R2_trimmed_minlen50.fastq | \
+   samtools view -Sb - > "$RESULTS_PATH$SAMPLE_NAME"_L002_unsorted.bam
+
+echo "\n####Merge bam for lanes"
+samtools merge "$RESULTS_PATH$SAMPLE_NAME"_unsorted.bam "$RESULTS_PATH$SAMPLE_NAME"_L001_unsorted.bam "$RESULTS_PATH$SAMPLE_NAME"_L002_unsorted.bam
+
+echo "\n####Sort bam file"
+samtools sort "$RESULTS_PATH$SAMPLE_NAME"_unsorted.bam -o "$RESULTS_PATH$SAMPLE_NAME"_sorted.bam
 
 echo "\n####Mark duplicates"
-java -Xmx4g -Xms4g -jar /home/magda/picard.jar MarkDuplicates VALIDATION_STRINGENCY=LENIENT INPUT="$RESULTS_PATH$SAMPLE_NAME".sorted.bam OUTPUT="$RESULTS_PATH$SAMPLE_NAME".bam METRICS_FILE="$RESULTS_PATH$SAMPLE_NAME"_picard_markduplicates_metrics.txt REMOVE_DUPLICATES=false ASSUME_SORTED=true
+java -Xmx10g -Xms10g -jar /home/magda/picard.jar MarkDuplicates VALIDATION_STRINGENCY=LENIENT INPUT="$RESULTS_PATH$SAMPLE_NAME".sorted.bam OUTPUT="$RESULTS_PATH$SAMPLE_NAME".bam METRICS_FILE="$RESULTS_PATH$SAMPLE_NAME"_picard_markduplicates_metrics.txt REMOVE_DUPLICATES=false ASSUME_SORTED=true
 
 echo "\n####Index bam file"
 samtools index  "$RESULTS_PATH$SAMPLE_NAME".bam
@@ -35,7 +61,7 @@ echo "\n####Extract basic mapping metrics"
 samtools flagstat "$RESULTS_PATH$SAMPLE_NAME".bam > "$RESULTS_PATH$SAMPLE_NAME"_samtools_flagstat_metrics.txt
 
 echo "\n####Estimate Insert Size Distribution"
-java -Xmx4g -jar /home/magda/picard.jar CollectInsertSizeMetrics VALIDATION_STRINGENCY=LENIENT HISTOGRAM_FILE="$RESULTS_PATH$SAMPLE_NAME"_picard_insert_size_plot.pdf INPUT="$RESULTS_PATH$SAMPLE_NAME".bam OUTPUT="$RESULTS_PATH$SAMPLE_NAME"_picard_insert_size_metrics.txt
+java -Xmx10g -jar /home/magda/picard.jar CollectInsertSizeMetrics VALIDATION_STRINGENCY=LENIENT HISTOGRAM_FILE="$RESULTS_PATH$SAMPLE_NAME"_picard_insert_size_plot.pdf INPUT="$RESULTS_PATH$SAMPLE_NAME".bam OUTPUT="$RESULTS_PATH$SAMPLE_NAME"_picard_insert_size_metrics.txt
 
 echo "\n####Count on target rate"
 bedtools intersect -bed -abam "$RESULTS_PATH$SAMPLE_NAME".bam -b "$PRIMARY_TARGET" | wc -l > "$RESULTS_PATH$SAMPLE_NAME"_primary_target_count
@@ -44,9 +70,19 @@ bedtools intersect -bed -abam "$RESULTS_PATH$SAMPLE_NAME".bam -b "$PADDED_CAPTUR
 
 echo "\n####Calculate depth of coverage"
 ##DepthOfCoverage is not present in GATK4 - GATK3 must be used
-java -Xmx4g -Xms4g -jar /home/magda/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar -T DepthOfCoverage -R /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa -I "$RESULTS_PATH$SAMPLE_NAME".bam -o "$RESULTS_PATH$SAMPLE_NAME"_gatk_primary_target_coverage -L "$PRIMARY_TARGET" -ct 1 -ct 10 -ct 20
+java -Xmx10g -Xms10g -jar /home/magda/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar \
+   -T DepthOfCoverage \
+   -R /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa \
+   -I "$RESULTS_PATH$SAMPLE_NAME".bam \
+   -o "$RESULTS_PATH$SAMPLE_NAME"_gatk_primary_target_coverage \
+   -L "$PRIMARY_TARGET" -ct 1 -ct 10 -ct 20 -nt 10
 
-java -Xmx4g -Xms4g -jar /home/magda/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar -T DepthOfCoverage -R /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa -I "$RESULTS_PATH$SAMPLE_NAME".bam -o "$RESULTS_PATH$SAMPLE_NAME"_gatk_capture_target_coverage -L "$CAPTURE_TARGET" -ct 1 -ct 10 -ct 20
+java -Xmx10g -Xms10g -jar /home/magda/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar \
+   -T DepthOfCoverage \
+   -R /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa \
+   -I "$RESULTS_PATH$SAMPLE_NAME".bam \
+   -o "$RESULTS_PATH$SAMPLE_NAME"_gatk_capture_target_coverage \
+   -L "$CAPTURE_TARGET" -ct 1 -ct 10 -ct 20 -nt 10
 
 echo "\n###Create Picard Interval Lists"
 samtools view -H "$RESULTS_PATH$SAMPLE_NAME".bam > "$RESULTS_PATH$SAMPLE_NAME"_bam_header.txt
@@ -60,4 +96,4 @@ cat "$RESULTS_PATH$SAMPLE_NAME"_bam_header.txt "$RESULTS_PATH$SAMPLE_NAME"_bait.
 cat "$RESULTS_PATH$SAMPLE_NAME"_bam_header.txt "$RESULTS_PATH$SAMPLE_NAME"_target.body.txt > "$RESULTS_PATH$SAMPLE_NAME"_target_intervals.txt
 
 echo "\n###Hybrid Selection analysis"
-java -Xmx4g -Xms4g -jar /home/magda/picard.jar CollectHsMetrics  BAIT_INTERVALS="$RESULTS_PATH$SAMPLE_NAME"_bait_intervals.txt TARGET_INTERVALS="$RESULTS_PATH$SAMPLE_NAME"_target_intervals.txt INPUT="$RESULTS_PATH$SAMPLE_NAME".bam OUTPUT="$RESULTS_PATH$SAMPLE_NAME"_picard_hs_metrics.txt METRIC_ACCUMULATION_LEVEL=ALL_READS REFERENCE_SEQUENCE=/mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa VALIDATION_STRINGENCY=LENIENT TMP_DIR=.
+java -Xmx10g -Xms10g -jar /home/magda/picard.jar CollectHsMetrics  BAIT_INTERVALS="$RESULTS_PATH$SAMPLE_NAME"_bait_intervals.txt TARGET_INTERVALS="$RESULTS_PATH$SAMPLE_NAME"_target_intervals.txt INPUT="$RESULTS_PATH$SAMPLE_NAME".bam OUTPUT="$RESULTS_PATH$SAMPLE_NAME"_picard_hs_metrics.txt METRIC_ACCUMULATION_LEVEL=ALL_READS REFERENCE_SEQUENCE=/mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa VALIDATION_STRINGENCY=LENIENT TMP_DIR=.
--- run_for_all_samples.sh
+++ run_for_all_samples.sh
@@ -2,7 +2,7 @@
 
 for l in /mnt/chr11/Data/magda/Powroty/panel/fastq/MARCEL*R1_001.fastq;
    do path=$(echo $l | cut -d/ -f9); 
-   sample=$(echo $path | cut -d_ -f1-3);
+   sample=$(echo $path | cut -d_ -f1-2);
    echo $sample;
    bash run_diagnostic_pipeline.sh $sample >> /mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/"$sample".log 2>&1 
    done
