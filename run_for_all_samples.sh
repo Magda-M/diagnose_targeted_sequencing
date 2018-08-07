c2a96f464de14346822f31b025124d983c95d622
Magda <m.machnicka@mimuw.edu.pl>
2018-08-07 12:52:31 +0200

    bug corrected

--- run_for_all_samples.sh
+++ run_for_all_samples.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for l in /mnt/chr11/Data/magda/Powroty/panel/fastq/MARCEL*R1_001.fastq;
+for l in /mnt/chr11/Data/magda/Powroty/panel/fastq/MARCEL*_L001_R1_001.fastq;
    do path=$(echo $l | cut -d/ -f9); 
    sample=$(echo $path | cut -d_ -f1-2);
-   echo $sample;
+   echo -e "\n$sample";
    bash run_diagnostic_pipeline.sh $sample &> /mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/merged/minlen50/"$sample".log 
    done
 
