e64d89abe34d3373e40a2e30081f9f011391289f
Magda <m.machnicka@mimuw.edu.pl>
2018-08-07 10:49:57 +0200

    corrected log location

--- run_for_all_samples.sh
+++ run_for_all_samples.sh
@@ -4,7 +4,7 @@ for l in /mnt/chr11/Data/magda/Powroty/panel/fastq/MARCEL*R1_001.fastq;
    do path=$(echo $l | cut -d/ -f9); 
    sample=$(echo $path | cut -d_ -f1-2);
    echo $sample;
-   bash run_diagnostic_pipeline.sh $sample >> /mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/"$sample".log 2>&1 
+   bash run_diagnostic_pipeline.sh $sample &> /mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/merged/minlen50/"$sample".log 
    done
 
 
