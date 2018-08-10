df3286e5664e8eeb38034cf5149d81e2ad22dec7
Magda <m.machnicka@mimuw.edu.pl>
2018-08-10 16:21:05 +0200

    bug

--- run_for_all_samples_Bowtie.sh
+++ run_for_all_samples_Bowtie.sh
@@ -5,7 +5,7 @@ RESULTS_PATH="$2"
 
 for l in "$FASTQ_PATH"/*_L001_R1_001.fastq;
    do path=$(echo ${l##*/});
-   sample=$(echo $path | cut -d_ -f1-3);
+   sample=$(echo $path | cut -d_ -f1-2);
    echo -e "\n$sample";
    bash run_diagnostic_pipeline_with_Bowtie.sh $sample $FASTQ_PATH $RESULTS_PATH &> "$RESULTS_PATH$sample".log 
    done
