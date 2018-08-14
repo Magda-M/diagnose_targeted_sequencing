512568c0aa82f7c35b18fb4697cbeb4f3c6e268a
Magda <m.machnicka@mimuw.edu.pl>
2018-08-14 10:57:48 +0200

    updated depthofcoverage

--- run_DepthOfCoverage.sh
+++ run_DepthOfCoverage.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-RESULTS_PATH="/mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/merged/minlen50/"
 PRIMARY_TARGET="/mnt/chr11/Data/magda/Powroty/panel/Symfonia_v2_primary_targets.bed"
 CAPTURE_TARGET="/mnt/chr11/Data/magda/Powroty/panel/Symfonia_v2_capture_targets.bed"
 
-for l in /mnt/chr11/Data/magda/Powroty/panel/fastq/MARCEL*_L001_R1_001.fastq;
-   do path=$(echo $l | cut -d/ -f9); 
+FASTQ_PATH="$1"
+RESULTS_PATH="$2"
+
+for l in "$FASTQ_PATH"/*_L001_R1_001.fastq;
+   do path=$(echo ${l##*/});
    SAMPLE_NAME=$(echo $path | cut -d_ -f1-2);
    echo -e "\n$SAMPLE_NAME";
+
 
    echo -e "\n####Calculate depth of coverage"
    ##DepthOfCoverage is not present in GATK4 - GATK3 must be used
    java -Xmx10g -Xms10g -jar /home/magda/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar \
    -T DepthOfCoverage \
-   -R /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa \
+   -R /home/magda//Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
    -I "$RESULTS_PATH$SAMPLE_NAME".bam \
    -o "$RESULTS_PATH$SAMPLE_NAME"_gatk_primary_target_coverage \
    -L "$PRIMARY_TARGET" \
-   -ct 1 -ct 10 -ct 20 >> /mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/merged/minlen50/"$SAMPLE_NAME".log 2>&1
+   -ct 1 -ct 10 -ct 20 >> "$RESULTS_PATH$SAMPLE_NAME".log 2>&1
 
    java -Xmx10g -Xms10g -jar /home/magda/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar \
    -T DepthOfCoverage \
-   -R /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa \
+   -R /home/magda//Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
    -I "$RESULTS_PATH$SAMPLE_NAME".bam \
    -o "$RESULTS_PATH$SAMPLE_NAME"_gatk_capture_target_coverage \
    -L "$CAPTURE_TARGET" \
-   -ct 1 -ct 10 -ct 20 >> /mnt/chr11/Data/magda/Powroty/panel/diagnose_sequencing/results/merged/minlen50/"$SAMPLE_NAME".log 2>&1
+   -ct 1 -ct 10 -ct 20 >> "$RESULTS_PATH$SAMPLE_NAME".log 2>&1
 
    done 
