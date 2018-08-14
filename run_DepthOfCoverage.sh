2da06ec41538855b6891a5e372ce8487c3ef3f08
Magda <m.machnicka@mimuw.edu.pl>
2018-08-14 11:12:50 +0200

    reordersam for depthofcoverage

--- run_DepthOfCoverage.sh
+++ run_DepthOfCoverage.sh
@@ -11,21 +11,28 @@ for l in "$FASTQ_PATH"/*_L001_R1_001.fastq;
    SAMPLE_NAME=$(echo $path | cut -d_ -f1-2);
    echo -e "\n$SAMPLE_NAME";
 
+   echo -e "\n#### Sort reads"
+   # (troubleshooting - GATK was complaining about incompatible contigs between bam and reference)
+   java -Xmx10g -Xms10g -jar /home/magda/picard.jar ReorderSam \
+   I="$RESULTS_PATH$SAMPLE_NAME".bam \
+   O="$RESULTS_PATH$SAMPLE_NAME"_reordered.bam \
+   R=/mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa \
+   CREATE_INDEX=TRUE
 
    echo -e "\n####Calculate depth of coverage"
    ##DepthOfCoverage is not present in GATK4 - GATK3 must be used
    java -Xmx10g -Xms10g -jar /home/magda/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar \
    -T DepthOfCoverage \
-   -R /home/magda//Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
-   -I "$RESULTS_PATH$SAMPLE_NAME".bam \
+   -R /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa \
+   -I "$RESULTS_PATH$SAMPLE_NAME"_reordered.bam \
    -o "$RESULTS_PATH$SAMPLE_NAME"_gatk_primary_target_coverage \
    -L "$PRIMARY_TARGET" \
    -ct 1 -ct 10 -ct 20 >> "$RESULTS_PATH$SAMPLE_NAME".log 2>&1
 
    java -Xmx10g -Xms10g -jar /home/magda/GenomeAnalysisTK-3.8-1-0-gf15c1c3ef/GenomeAnalysisTK.jar \
    -T DepthOfCoverage \
-   -R /home/magda//Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
-   -I "$RESULTS_PATH$SAMPLE_NAME".bam \
+   -R /mnt/chr11/Data/magda/Powroty/panel/ref/hg38.fa \
+   -I "$RESULTS_PATH$SAMPLE_NAME"_reordered.bam \
    -o "$RESULTS_PATH$SAMPLE_NAME"_gatk_capture_target_coverage \
    -L "$CAPTURE_TARGET" \
    -ct 1 -ct 10 -ct 20 >> "$RESULTS_PATH$SAMPLE_NAME".log 2>&1
